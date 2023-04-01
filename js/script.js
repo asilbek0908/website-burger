28490f97826370bfbc3e3796e5b748fb8ff1e6ee
asilbek0908 <saidovasilbek2023@gmail.com>
2023-04-01 20:27:21 +0300

    commit

---
+++ js/script.js
@@ -0,0 +1,183 @@
+"use strict";
+
+
+const searchBtn = document.querySelector("#search__btn");
+const searchFrom = document.querySelector(".form__search");
+const navbar = document.querySelector(".navbar");
+const menuBtn =document.querySelector("#menu__btn");
+const addRate1 =document.querySelector(".star1")
+const addRate2 =document.querySelector(".star2")
+const addRate3 =document.querySelector(".star3")
+const addRate4 =document.querySelector(".star4")
+const addRate5 =document.querySelector(".star5")
+
+const addRate6 =document.querySelector(".star6")
+const addRate7 =document.querySelector(".star7")
+const addRate8 =document.querySelector(".star8")
+const addRate9 =document.querySelector(".star9")
+const addRate10 =document.querySelector(".star10")
+
+const addRate11 =document.querySelector(".star11")
+const addRate12 =document.querySelector(".star12")
+const addRate13 =document.querySelector(".star13")
+const addRate14 =document.querySelector(".star14")
+const addRate15 =document.querySelector(".star15")
+
+const addRate16 =document.querySelector(".star16")
+const addRate17 =document.querySelector(".star17")
+const addRate18 =document.querySelector(".star18")
+const addRate19 =document.querySelector(".star19")
+const addRate20 =document.querySelector(".star20")
+
+const addRate21 =document.querySelector(".star21")
+const addRate22 =document.querySelector(".star22")
+const addRate23 =document.querySelector(".star23")
+const addRate24 =document.querySelector(".star24")
+const addRate25 =document.querySelector(".star25")
+
+const addRate26 =document.querySelector(".star26")
+const addRate27 =document.querySelector(".star27")
+const addRate28 =document.querySelector(".star28")
+const addRate29 =document.querySelector(".star29")
+const addRate30 =document.querySelector(".star30")
+
+searchBtn.addEventListener("click", ()=>{
+    searchBtn.classList.toggle("fa-times");
+    searchFrom.classList.toggle("active");
+    menuBtn.classList.remove("fa-times");
+    navbar.classList.remove("active");
+})
+    
+menuBtn.addEventListener("click", ()=>{
+    menuBtn.classList.toggle("fa-times")
+    navbar.classList.toggle("active");
+    searchBtn.classList.remove("fa-times");
+    searchFrom.classList.remove("active");
+})
+
+
+window.onscroll = ()=>{
+    searchBtn.classList.remove("fa-times");
+    searchFrom.classList.remove("active");
+    menuBtn.classList.remove("fa-times");
+    navbar.classList.remove("active");
+}
+
+
+
+addRate1.addEventListener("click", ()=>{
+    addRate1.classList.toggle("rate")
+})
+
+addRate2.addEventListener("click", ()=>{
+    addRate2.classList.toggle("rate")
+})
+addRate3.addEventListener("click", ()=>{
+    addRate3.classList.toggle("rate")
+})
+addRate4.addEventListener("click", ()=>{
+    addRate4.classList.toggle("rate")
+})
+addRate5.addEventListener("click", ()=>{
+    addRate5.classList.toggle("rate")
+})
+
+
+
+
+
+addRate6.addEventListener("click", ()=>{
+    addRate6.classList.toggle("rate")
+})
+
+addRate7.addEventListener("click", ()=>{
+    addRate7.classList.toggle("rate")
+})
+addRate8.addEventListener("click", ()=>{
+    addRate8.classList.toggle("rate")
+})
+addRate9.addEventListener("click", ()=>{
+    addRate9.classList.toggle("rate")
+})
+addRate10.addEventListener("click", ()=>{
+    addRate10.classList.toggle("rate")
+})
+
+
+
+
+addRate11.addEventListener("click", ()=>{
+    addRate11.classList.toggle("rate")
+})
+
+addRate12.addEventListener("click", ()=>{
+    addRate12.classList.toggle("rate")
+})
+addRate13.addEventListener("click", ()=>{
+    addRate13.classList.toggle("rate")
+})
+addRate14.addEventListener("click", ()=>{
+    addRate14.classList.toggle("rate")
+})
+addRate15.addEventListener("click", ()=>{
+    addRate15.classList.toggle("rate")
+})
+
+
+
+
+addRate16.addEventListener("click", ()=>{
+    addRate16.classList.toggle("rate")
+})
+
+addRate17.addEventListener("click", ()=>{
+    addRate17.classList.toggle("rate")
+})
+addRate18.addEventListener("click", ()=>{
+    addRate18.classList.toggle("rate")
+})
+addRate19.addEventListener("click", ()=>{
+    addRate19.classList.toggle("rate")
+})
+addRate20.addEventListener("click", ()=>{
+    addRate20.classList.toggle("rate")
+})
+
+
+
+
+addRate21.addEventListener("click", ()=>{
+    addRate21.classList.toggle("rate")
+})
+
+addRate22.addEventListener("click", ()=>{
+    addRate22.classList.toggle("rate")
+})
+addRate23.addEventListener("click", ()=>{
+    addRate23.classList.toggle("rate")
+})
+addRate24.addEventListener("click", ()=>{
+    addRate24.classList.toggle("rate")
+})
+addRate25.addEventListener("click", ()=>{
+    addRate25.classList.toggle("rate")
+})
+
+
+
+addRate26.addEventListener("click", ()=>{
+    addRate26.classList.toggle("rate")
+})
+
+addRate27.addEventListener("click", ()=>{
+    addRate27.classList.toggle("rate")
+})
+addRate28.addEventListener("click", ()=>{
+    addRate28.classList.toggle("rate")
+})
+addRate29.addEventListener("click", ()=>{
+    addRate29.classList.toggle("rate")
+})
+addRate30.addEventListener("click", ()=>{
+    addRate30.classList.toggle("rate")
+})
